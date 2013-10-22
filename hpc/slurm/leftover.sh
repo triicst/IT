eeaa832ab666f8ff6d6f09d8e90cb7f2abc0bd46
Michael Gutteridge <michael.gutteridge@gmail.com>
2013-10-22 11:27:14 -0700

    Major update
    - Kill behavior now different when run in terminal vs. cron
    - updated list of reserved nodes
    - updated squeue args to use -w for requesting nodes (-n deprecated)
    - notifications sent to syslog
    - use TERM before KILL when killing jobs

--- hpc/slurm/leftover.sh
+++ hpc/slurm/leftover.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
-KILL=true
+logger -i -t leftover " started on `date`"
+
+# This parameter governs how long between sending the default
+# signale and SIGKILL to rogue processes
+TIMEOUT=90
+
+KILL=false
 while [ $# -gt 0 ] ; do
-	case $1 in
-		-k) KILL=true ; shift 1 ;;
-		*) shift 1 ;;
-	esac
+    case $1 in
+        -k) KILL=true ; shift 1 ;;
+        *) shift 1 ;;
+    esac
 done
 
-reserved="daemon haldaemon messagebus ntp nobody root postfix man sshd avahi ldap slurm wwwrun at"
+# Also kill if run in a non-interactive session (e.g. cron)
+# If stdout is closed, then we are in a non-interactive session
+if [ \! -t 0 ]
+then
+    KILL=true
+fi
+
+reserved="avahi daemon ganglia haldaemon halevt lp messagebus munge nobody ntp postfix root statd syslog www-data landscape sshd"
 
 # users supposed to be running on node
-queued=`squeue -a -h -n \`hostname\` -o "%u"|sort -u`
+queued=`squeue -a -h -w \`hostname\` -o "%u"|sort -u`
 #echo "queued: $queued"
 
 # users actually running on node
@@ -22,23 +35,26 @@ total="$reserved $queued"
 
 for i in $actual
 do
-	MATCH=false
-	for j in $total
-	do
-		if [ ${j} == ${i} ] ; then
-			MATCH=true
-			break
-		fi
-	done
-	if ! ${MATCH} ; then
-		if ${KILL} ; then
-			echo "`date`: killing all jobs owned by ${i}"
-			killall -9 -v -u ${i}
-		else
-			echo "${i} shouldn't be running"
-		fi
-	fi
+    MATCH=false
+    for j in $total
+    do
+        if [ ${j} == ${i} ] ; then
+            MATCH=true
+            break
+        fi
+    done
+    if ! ${MATCH} ; then
+        if ${KILL} ; then
+            logger -i -t leftover " killing all jobs owned by ${i}"
+            killall -u ${i}
+            sleep ${TIMEOUT}
+            killall -9 -u ${i}
+        else
+            logger -i -t leftover " ${i} shouldn't be running"
+        fi
+    fi
 done
 
-# prune files/directories not accessed in last day
+# prune files/directories not accessed in last 7 days
 find /tmp -atime +7 -print0 | xargs -0 /bin/rm -rf
+
