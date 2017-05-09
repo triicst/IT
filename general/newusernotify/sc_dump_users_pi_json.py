9d5b05180c9110fc3ca35ff4ed4031c09a035d93
root <root@fox.fhcrc.org>
2017-05-08 23:25:26 -0700

    changed user filter from db

--- general/newusernotify/sc_dump_users_pi_json.py
+++ general/newusernotify/sc_dump_users_pi_json.py
@@ -43,7 +43,7 @@ into SCICOMP.dbo.SC_users
 from usersfiltered
 INNER JOIN SCICOMP.dbo.SC_CostCenters
 ON usersfiltered.deptID = SCICOMP.dbo.SC_CostCenters.dept_id
-where (paygroup <> 'CCA' and status = 'A')
+where (paygroup <> 'CCA' and status <> 'T')
 """)
 
 conn.commit()
@@ -80,11 +80,105 @@ for row in rows:
 	d['department'] = row[20]
 	objects_list.append(d)
  
-j = json.dumps(objects_list, indent=4)
+j = json.dumps(objects_list, indent=4, default=lambda x:str(x))
 
 with open(outfolder + '/sc_users.json', 'w') as f:
 	f.write(j)
 
+# *********************** SC_CostCenters_all *****************************
+
+cursor = conn.cursor()
+cursor.execute("""
+DROP TABLE SC_CostCenters_all;
+select
+  DEPTID as dept_id,
+  DESCRSHORT as pi_dept,
+  COMPANY as organization,
+  DESCR as department
+into SCICOMP.dbo.SC_CostCenters_all
+from SCICOMP.dbo.PS_DEPT_TBL
+where (COMPANY <> 'CCA') ;
+""")
+
+conn.commit()
+
+# ****************************** ALL_users ********************************
+### DROP TABLE SC_users_all
+
+cursor = conn.cursor()
+cursor.execute("""
+DROP TABLE SC_users_all
+;with usersfiltered as (
+  select
+    EMPLID as employeeID,
+    FH_CENTERALIAS as uid,
+    NAME as displayName,
+    FH_EMAIL_ADDR1 as mail,
+    BUSINESS_TITLE as title,
+    FH_DIVISION as division,
+    FH_SUPV_NAME as mgrName,
+    case when PREFERRED_NAME = '' or PREFERRED_NAME is null
+    then FIRST_NAME
+    else PREFERRED_NAME collate Latin1_General_BIN
+    end as givenName,
+    LAST_NAME as sn,
+    REHIRE_DT as hireDate,
+    DEPTID as deptID,
+    SUPERVISOR_ID as mgrID,
+    PAYGROUP as paygroup,
+    JOB_FUNCTION as jobFunc,
+    JOBTITLE as jobTitle,
+    EMPL_STATUS as status,
+    row_number() over(partition by EMPLID order by EMPL_RCD desc) as RowNum
+    from SCICOMP.dbo.PS_FH_STAFF
+)
+select *
+into SCICOMP.dbo.SC_users_all
+from usersfiltered
+INNER JOIN SCICOMP.dbo.SC_CostCenters_all
+ON usersfiltered.deptID = SCICOMP.dbo.SC_CostCenters_all.dept_id
+where (paygroup <> 'CCA' and status <> 'T')
+""")
+
+
+conn.commit()
+
+cursor = conn.cursor()
+cursor.execute('SELECT * FROM SC_users_all')
+
+rows = cursor.fetchall()
+
+objects_list = []
+objects_list_today = []
+for row in rows:
+        d = collections.OrderedDict()
+        d['employeeID'] = row[0]
+        d['uid'] = row[1]
+        d['displayName'] = row[2]
+        d['mail'] = row[3]
+        d['title'] = row[4]
+        d['division'] = row[5]
+        d['mgrName'] = row[6]
+        d['givenName'] = row[7].strip()
+        d['sn'] = row[8]
+        d['hireDate'] = row[9].isoformat()
+        #d['deptID'] = row[10]
+        d['mgrID'] = row[11]
+        d['paygroup'] = row[12]
+        d['jobFunc'] = row[13]
+        d['jobTitle'] = row[14]
+        d['status'] = row[15]
+        #d['RowNum'] = row[16]
+        d['dept_id'] = row[17]
+        d['pi_dept'] = row[18]
+        d['dept_manager'] = row[19]
+        d['department'] = row[20]
+        objects_list.append(d)
+
+j = json.dumps(objects_list, indent=4, default=lambda x:str(x))
+
+with open(outfolder + '/users_all.json', 'w') as f:
+        f.write(j)
 
 # ****************************** SC_pi_all ********************************
 cursor = conn.cursor()
@@ -95,6 +189,7 @@ select
   then substring(lower(FIRST_NAME),1,1)
   else substring(lower(PREFERRED_NAME),1,1) collate Latin1_General_BIN
   end) as pi_dept,
+  EMPLID as employeeID,
   FH_CENTERALIAS as uid,
   "NAME" as displayName,
   LAST_NAME as sn,
@@ -104,18 +199,23 @@ select
   end as givenName,
   FH_EMAIL_ADDR1 as mail,
   JOBTITLE as jobTitle,
+  BUSINESS_TITLE as businessTitle,
+  FH_SUPV_NAME as mgrName,
   DESCR as department,
   FH_DIVISION as division
 into SCICOMP.dbo.SC_pi_all
 from SCICOMP.dbo.PS_FH_STAFF
-where (((JOB_FUNCTION = 'FAC' and PAYGROUP = 'FHC') or JOBTITLE = 'Full Member') and EMPL_STATUS = 'A')
-group by DEPTID, FH_CENTERALIAS, PREFERRED_NAME, "NAME", LAST_NAME, FIRST_NAME, FH_EMAIL_ADDR1, JOBTITLE, DESCR, FH_DIVISION
+where (((JOB_FUNCTION = 'FAC' and PAYGROUP = 'FHC') or JOBTITLE = 'Full Member') and EMPL_STATUS <> 'T')
+group by DEPTID, EMPLID, FH_CENTERALIAS, PREFERRED_NAME, "NAME", LAST_NAME, FIRST_NAME, FH_EMAIL_ADDR1, JOBTITLE, BUSINESS_TITLE, FH_SUPV_NAME, DESCR, FH_DIVISION
 """)
 
 conn.commit()
 
+
+#### 
+
 cursor = conn.cursor()
-cursor.execute('SELECT * FROM SC_pi_all')
+cursor.execute('SELECT * FROM SC_pi_all UNION SELECT * FROM SC_pi_extra order by employeeID')
 
 rows = cursor.fetchall()
 
@@ -124,20 +224,22 @@ objects_list_today = []
 for row in rows:
         d = collections.OrderedDict()
         d['pi_dept'] = row[0]
-        d['uid'] = row[1]
-        d['displayName'] = row[2]
-        d['sn'] = row[3]
-        d['givenName'] = row[4].strip()
-        d['mail'] = row[5]
-        d['jobtitle'] = row[6]
-        d['department'] = row[7]
-        d['division'] = row[8]
+        d['employeeID'] = row[1]
+        d['uid'] = row[2]
+        d['displayName'] = row[3]
+        d['sn'] = row[4]
+        d['givenName'] = row[5].strip()
+        d['mail'] = row[6]
+        d['jobTitle'] = row[7]
+        d['businessTitle'] = row[8]
+        d['mgrName'] = row[9]
+        d['department'] = row[10]
+        d['division'] = row[11]
         objects_list.append(d)
 
 conn.close()
 
-j = json.dumps(objects_list, indent=4)
-
+j = json.dumps(objects_list, indent=4, default=lambda x:str(x))
 with open(outfolder + '/pi_all.json', 'w') as f:
         f.write(j)
 
