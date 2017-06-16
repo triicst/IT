c8076d2768a5a73e89367ec1da69275252127d40
Robert McDermott <robert.c.mcdermott@gmail.com>
2017-06-16 16:26:59 -0700

    added command line arguments and Linux binary for download

--- filetools/godna/README.md
+++ filetools/godna/README.md
@@ -1,3 +1,28 @@
 # Godna DNA generator
 
 Godna is a storage benchmarking tool that generates a random DNA sequence and writes it concurently to multiple files.  
+
+##Usage
+
+```
+./godna [-n <number of files>  -t <cpu threads> -c <concurrecy> -d </dir/path>] -s <size in bytes>
+```
+
+##Arguments
+
+  -c int
+    	Optional: set number of concurrent file writers to use; defaults to the number of logical CPUs in your system (default 56)
+  -d string
+    	Optional: Directory to write the files; defaults to the current directory (default ".")
+  -n int
+    	Optional: set the number of files; defaults to 1 file (default 1)
+  -s int
+    	Required: the size of the file(s) to generate in Bytes
+  -t int
+    	Optional: set number CPU threads available for use; defaults to the number of logical CPUs in your system (default 56)
+
+##Example
+
+```
+./godna -n 1024 -t 16 -c 32 -s 10485760 -d /tmp
+```
--- filetools/godna/main.go
+++ filetools/godna/main.go
@@ -2,25 +2,48 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"math/rand"
 	"os"
-	"sync"
 	"runtime"
+	"sync"
 	"time"
 )
 
 func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
-	size := 1024 * 1024 * 10 
-        wg := new(sync.WaitGroup)
-        sema := make(chan struct{}, runtime.NumCPU())
+
+	// define and set default command parameter flags
+	var nFlag = flag.Int("n", 1, "Optional: set the number of files; defaults to 1 file")
+	var tFlag = flag.Int("t", runtime.NumCPU(), "Optional: set number CPU threads available for use; defaults to the number of logical CPUs in your system")
+	var cFlag = flag.Int("c", runtime.NumCPU(), "Optional: set number of concurrent file writers to use; defaults to the number of logical CPUs in your system")
+	var sFlag = flag.Int("s", 0, "Required: the size of the file(s) to generate in Bytes")
+	var dFlag = flag.String("d", ".", "Optional: Directory to write the files; defaults to the current directory")
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "\nUsage: %s [-n <number of files>  -t <cpu threads> -c <concurrecy> -d </dir/path>] -s <size in bytes>\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "\nExample: %s -n 1024 -t 16 -c 32 -s 10485760 -d /tmp\n\n", os.Args[0])
+		flag.PrintDefaults()
+		fmt.Println()
+	}
+	flag.Parse()
+
+	if *sFlag == 0 {
+		fmt.Fprintf(os.Stderr, "\nMissing required -s (size) argument\n\n")
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	runtime.GOMAXPROCS(*tFlag)
+	size := *sFlag
+	wg := new(sync.WaitGroup)
+	sema := make(chan struct{}, *cFlag)
 	out := genstring(size)
 
-        for x := 1; x <= 5120; x++ {
-                wg.Add(1)
-                go spraydna(x, wg, sema, &out)
-        }
+	for x := 1; x <= *nFlag; x++ {
+		wg.Add(1)
+		go spraydna(x, wg, sema, &out, *dFlag)
+	}
 	wg.Wait()
 }
 
@@ -36,22 +59,22 @@ func genstring(size int) []byte {
 }
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
-func spraydna(count int, wg *sync.WaitGroup, sema chan struct{}, out *[]byte) {
-        defer wg.Done()
-        sema <- struct{}{}        // acquire token
-        defer func() { <-sema }() // release token
-	filename := fmt.Sprintf("dna/dna-%d.txt", count)
+func spraydna(count int, wg *sync.WaitGroup, sema chan struct{}, out *[]byte, dir string) {
+	defer wg.Done()
+	sema <- struct{}{}        // acquire token
+	defer func() { <-sema }() // release token
+	filename := fmt.Sprintf("%s/dna-%d.txt", dir, count)
 	f, err := os.Create(filename)
-        check(err)
+	check(err)
 	defer f.Close()
-	
+
 	w := bufio.NewWriter(f)
-    	w.Write(*out)
-    	//fmt.Printf("wrote %d bytes\n", o)
+	w.Write(*out)
+	//fmt.Printf("wrote %d bytes\n", o)
 	w.Flush()
 }
