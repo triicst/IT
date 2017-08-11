ffb0f2aedb1188b81455867203a068c482155d9b
dirkpetersen <petersen@fredhutch.org>
2017-08-11 11:55:07 -0700

    fix for < python3.5

--- general/newusernotify/sc_dump_groups_json.py
+++ general/newusernotify/sc_dump_groups_json.py
@@ -4,13 +4,14 @@ import sys, os, requests, csv, json, collections, subprocess
 
 outfolder = '/var/www/toolbox'
 
-ret = subprocess.run('getent group | grep _grp', stdout=subprocess.PIPE, 
-                   stderr=subprocess.PIPE, shell=True, universal_newlines=True)
+# Only works with Python3.5+
+#ret = subprocess.call('getent group | grep _grp', stdout=subprocess.PIPE, 
+#                   stderr=subprocess.PIPE, shell=True, universal_newlines=True)
 
-# print(ret.returncode, ret.stdout, ret.stderr)
+ret = subprocess.check_output('getent group | grep _grp', shell=True, universal_newlines=True) 
 
 groups={}
-grouplines=ret.stdout.split('\n')
+grouplines=ret.split('\n')
 for groupline in grouplines:
     grouplist=groupline.split(':')
     if grouplist[0]!='':
