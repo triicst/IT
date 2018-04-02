9b3a39f8269913c50920286e441c4d0291e1efbe
dirkpetersen <petersen@fhcrc.org>
2018-04-02 16:46:48 -0700

    works in python2 and python3

--- py/pub2graph.py
+++ py/pub2graph.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python
 
 import os, sys, subprocess, argparse, csv, json, requests, os
 from collections import OrderedDict
@@ -11,8 +11,8 @@ def main():
     j = getToolbox('https://toolbox.fhcrc.org/json/faculty.json')
 
     namein=args.csvfile
-    nameout=os.path.splitext(namein)[0]+'-graph.dv'
-    nameoutpdf=os.path.splitext(namein)[0]+'-graph.pdf'
+    nameout=os.path.splitext(namein)[0]+'.dv'
+    nameoutpdf=os.path.splitext(namein)[0]+'.pdf'
 
     pairs={}
 
@@ -71,8 +71,9 @@ def main():
         outfile.write('}\n')
 
         # unflatten -f -l 4 -c 6 input.dot | dot | gvpack -array_t6 | neato -s -n2 -Tpng -o output.png
-        ret = subprocess.run("unflatten -f -l20 -c1 %s | dot -T pdf -o %s"
-            % (nameout, nameoutpdf), shell=True)
+        ret = subprocess.Popen("unflatten -f -l12 -c12 %s | dot -T pdf -o %s"
+            % (nameout, nameoutpdf), shell=True, stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+
 
 def getDisplayNames(j, authors):
     #names=authors.split(';')
@@ -144,95 +145,6 @@ def uniq(seq):
     for e in seq:
         keys[e] = 1
     return list(keys.keys())
-
-
-
-    #pgpass = os.getenv('PGPASSFILE', os.path.expanduser('~/.pgpass'))
-    #if not os.path.exists(pgpass):
-        #print('You need to create file ~/.pgpass that contains at least one line:')
-        #print('hostname:port:database:username:password')
-        #print('and has restricted permissions: chmod 600 ~/.pgpass')
-        #print('Also please check https://www.postgresql.org/docs/current/static/libpq-pgpass.html')
-        #return False
-
-    ##args.dsn == 'postgresql://dirk@mydb:32063/petersen'
-    ##args.csvfile = '/home/petersen/sc/data/slurm_jobs.csv'
-
-    #if not args.dsn.startswith('postgresql://'):
-        #dl = args.dsn.split(':')
-        #args.dsn = 'postgresql://%s@%s:%s/%s' % (dl[3], dl[0], dl[1], dl[2])
-
-    #try:
-        #conn = psycopg2.connect(args.dsn)
-        #cur = conn.cursor()
-    #except (Exception, psycopg2.DatabaseError) as error:
-        #print('Database error:', error)
-        #return False
-
-    #with open(args.csvfile, 'rb') as fh:
-        #table_set = CSVTableSet(fh)
-        #row_set = table_set.tables[0]
-        ##print row_set.sample.next()
-        #offset, headers = headers_guess(row_set.sample)
-        #row_set.register_processor(headers_processor(headers))
-        #row_set.register_processor(offset_processor(offset + rowtocheck))
-        #types = type_guess(row_set.sample, strict=True)
-
-    #myd = dict (zip (headers, types))
-    #print("\nDetected columns & types:\n", myd, '\n')
-
-    #table = os.path.splitext(os.path.basename(args.csvfile))[0]
-    #table = table.replace('-','_')
-    #table = table.replace(' ','_')
-    #create_sql = "CREATE TABLE %s (" % table
-    #idh = 0
-    #for h in headers:
-        #myt = "TEXT"
-        #if str(types[idh]) == 'Integer':
-            #myt = 'BIGINT'
-        #elif str(types[idh]) == 'Bool':
-             #myt = 'BIGINT'
-        #elif str(types[idh]) == 'Decimal':
-             #myt = 'DECIMAL'
-
-        #create_sql += "%s %s, " % (h,myt)
-        #idh += 1
-    #create_sql = create_sql[:-2] + ');'
-
-    #print("\ncreating postgres table '%s':\n" % table, create_sql, '\n')
-    #try:
-        #if args.overwrite:
-            #drop_sql = 'DROP TABLE IF EXISTS %s' % table
-            #cur.execute(drop_sql)
-            #conn.commit()
-        #cur.execute(create_sql)
-        #conn.commit()
-    #except (Exception, psycopg2.DatabaseError) as error:
-        #print('Database error:', error)
-        #sys.exit()
-
-    #print("\nloading data .... ")
-
-    #with open(args.csvfile, 'rb') as fh:
-        #sample_text = ''.join(str(fh.readline()) for x in range(3))
-        #try:
-            #dialect = csv.Sniffer().sniff(sample_text)
-            #if dialect.delimiter == 't':
-                #delim = '\t'
-            #else:
-                #delim = dialect.delimiter
-        #except:
-            #delim = ","
-        #copy_sql = "COPY %s FROM stdin WITH CSV HEADER DELIMITER as '%s'" % \
-                   #(table, delim)
-        #try:
-            #cur.copy_expert(sql=copy_sql, file=fh)
-            #conn.commit()
-            #cur.close()
-        #except (Exception, psycopg2.DatabaseError) as error:
-            #print('Database error:', error)
-
-    #print('Done !')
 
 
 def parse_arguments():
